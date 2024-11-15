1ed531095eb741ce33583fa05b8dd9e02e5bead5
Hallan Abreu <jotahagar@gmail.com>
2024-11-15 12:57:35 -0300

    aula Flutter 02 /02

--- lib/features/login/view/login_view.dart
+++ lib/features/login/view/login_view.dart
@@ -43,56 +43,60 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.green,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 60),
-                child: Text("Flutter Brasil", style: Theme.of(context).textTheme.displayMedium),
-              ),
-              Column(
+      body: AnimatedBuilder(
+        animation: loginController,
+        builder: (context, controller) {
+          return Container(
+            color: Colors.green,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Align(alignment: Alignment.centerLeft ,child: Text("Email", style: Theme.of(context).textTheme.displayLarge)),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    controller: loginController.emailController,
-                  ),
-                  const SizedBox(height: 20),
-                  Align(alignment: Alignment.centerLeft ,child: Text("Senha", style: Theme.of(context).textTheme.displayLarge)),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    controller: loginController.passwordController,
-                  ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: InkWell(
-                      onTap: ()=> Navigator.pushNamed(context, "/register"),
-                      child: Text("Criar conta", style: Theme.of(context).textTheme.titleLarge)),
+                    padding: const EdgeInsets.only(top: 60),
+                    child: Text("Flutter Brasil", style: Theme.of(context).textTheme.displayMedium),
                   ),
+                  Column(
+                    children: [
+                      Align(alignment: Alignment.centerLeft ,child: Text("Email", style: Theme.of(context).textTheme.displayLarge)),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: loginController.emailController,
+                      ),
+                      const SizedBox(height: 20),
+                      Align(alignment: Alignment.centerLeft ,child: Text("Senha", style: Theme.of(context).textTheme.displayLarge)),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: loginController.passwordController,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30),
+                        child: InkWell(
+                          onTap: ()=> Navigator.pushNamed(context, "/register"),
+                          child: Text("Criar conta", style: Theme.of(context).textTheme.titleLarge)),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(onPressed: ()async{
+                    if(loginController.isLoand) return;
+                    loginController.gerenciadorDeEstado(true);
+                    bool teste = await datasource();
+                    print(teste);
+                    loginController.gerenciadorDeEstado(false);
+                    if(teste){
+                      Navigator.pushNamed(context, "/home", arguments: 30);
+                    }else{
+                      print('Erro ao logar');
+                    }
+                  }, child: loginController.isLoand
+                   ?const CircularProgressIndicator()
+                   :const Text("Login")),
                 ],
               ),
-              ElevatedButton(onPressed: ()async{
-                if(loginController.isLoand) return;
-                loginController.gerenciadorDeEstado(true);
-                bool teste = await datasource();
-                print(teste);
-                loginController.gerenciadorDeEstado(false);
-                if(teste){
-                  Navigator.pushNamed(context, "/home", arguments: 30);
-                }else{
-                  print('Erro ao logar');
-                }
-                
-              }, child: loginController.isLoand
-               ?const CircularProgressIndicator()
-               :const Text("Login")),
-            ],
-          ),
-        ),
+            ),
+          );
+        }
       ),
     );
   }
